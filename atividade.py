328a0870459e5ad7e93b874fe6abf69e0030de29
BrunoMatheus10 <brunomatheus88300432@gmail.com>
2024-09-17 19:28:35 -0300

    Add files via upload

---
+++ atividade.py
@@ -0,0 +1,9 @@
+# Inicializando as listas A e B com 5 elementos cada
+A = [1, 2, 3, 4, 5]
+B = [6, 7, 8, 9, 10]
+
+# Trocando os elementos das listas A e B
+A, B = B, A
+
+print("Lista A após a troca:" ,A)
+print("Lista B após a troca:" ,B)
